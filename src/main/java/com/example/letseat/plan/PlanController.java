dc09a6b2ab66e33b5b76127601fef247c08c1263
hyunmin2 <201802142@o.cnu.ac.kr>
2023-09-13 13:17:54 +0900

    plan 생성 기능 추가

--- src/main/java/com/example/letseat/plan/PlanController.java
+++ src/main/java/com/example/letseat/plan/PlanController.java
@@ -1,15 +1,19 @@
 package com.example.letseat.plan;
 
+import com.example.letseat.plan.data.PlanRequest;
 import com.google.zxing.WriterException;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 @Controller
 @AllArgsConstructor
@@ -23,5 +27,14 @@ public class PlanController {
         String qrcode = planService.generateQR(planRequest);
 
         return ResponseEntity.ok(qrcode);
+    }
+
+    @PostMapping("/plan")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void plan(@RequestBody @Valid PlanRequest planRequest) {
+        Long senderId = planRequest.getSender_id();
+        Long receiverId = planRequest.getReceiver_id();
+        LocalDate expiredDate = planRequest.getExpired_date();
+        planService.savePlan(senderId, receiverId, expiredDate);
     }
 }
