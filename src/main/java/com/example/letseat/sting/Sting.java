58508c560a5649d097afd903dc40ca3b351faaec
hyunmin2 <201802142@o.cnu.ac.kr>
2023-10-04 18:18:39 +0900

    sting table 생성

---
+++ src/main/java/com/example/letseat/sting/Sting.java
@@ -0,0 +1,29 @@
+package com.example.letseat.sting;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Sting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long senderId;
+
+    @Column(nullable = false)
+    private Long receiverId;
+
+    @Column(nullable = false)
+    private Long planId;
+
+    @Column(nullable = false)
+    private LocalDateTime stingDate;
+
+}
